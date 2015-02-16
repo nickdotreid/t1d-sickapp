5dd5c5be3407e1eae30f1d95ab98affe549fb6ee
Nick Reid <nickreid@nickreid.com>
2015-02-15 21:00:25 -0500

    added iamges

--- js/main.js
+++ js/main.js
@@ -9,6 +9,20 @@ $( window ).resize(function() {
   	});
   	var bttnSize = ($('.container', actions).width() - btnWidth)/$('.btn',actions).length;
   	btns.width(bttnSize);
+
+  	var iconsAligned = $('.icon-aligned', actions);
+  	if(iconsAligned.length < 0) return 0;
+
+  	iconsAligned.css({
+  		display:'block',
+  		position:'absolute',
+  		top:$('.content').outerHeight() - actions.position().top,
+  		left:function(){
+  			var pos = (bttnSize-$(this).width())/2;
+  			if(pos < 1) return 0;
+  			return pos;
+  		},
+  	});
   });
 });
 
