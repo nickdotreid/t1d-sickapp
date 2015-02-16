8fd4f1cc3c05046469c9f8e479fb12774d629d49
Nick Reid <nickreid@nickreid.com>
2015-02-15 20:17:31 -0500

    added styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$( window ).resize(function() {
+  $('.actions').each(function(){
+  	var actions = $(this);
+  	var btns = $('.btn',actions);
+  	if (btns.length < 1) return 0;
+  	var btnWidth = 0;
+  	btns.width('auto').each(function(){
+  		btnWidth += $(this).width();
+  	});
+  	var bttnSize = ($('.container', actions).width() - btnWidth)/$('.btn',actions).length;
+  	btns.width(bttnSize);
+  });
+});
 
+
+$(document).ready(function(){
+	$(window).resize();
+});
