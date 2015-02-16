786e2a4f901f799bdb4673779afa754b42a32ae5
Nick Reid <nickreid@nickreid.com>
2015-02-16 12:52:57 -0500

    tweaked layout

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ $( window ).resize(function() {
   	btns.width('auto').each(function(){
   		btnWidth += $(this).width();
   	});
-  	var bttnSize = ($('.container', actions).width() - btnWidth)/$('.btn',actions).length;
-  	btns.width(bttnSize);
+  	var bttnSize = $('.container', actions).width()/$('.btn',actions).length;
+  	btns.width(bttnSize-1);
 
   	var iconsAligned = $('.icon-aligned', actions);
   	if(iconsAligned.length < 0) return 0;
@@ -19,7 +19,18 @@ $( window ).resize(function() {
   		top:$('.content').outerHeight() - actions.position().top,
   		left:function(){
   			var pos = (bttnSize-$(this).width())/2;
-  			if(pos < 1) return 0;
+  			//if(pos < 1) return 0;
+  			return pos;
+  		},
+  	})
+  	var dottedLines = $('.dotted-line',actions);
+  	if(dottedLines.length < 1) var dottedLines = $('<div class="dotted-line"></div>').insertAfter(iconsAligned);
+  	dottedLines.css({
+  		height:Math.abs($('.content').outerHeight() - actions.position().top) - 10,
+  		top:$('.content').outerHeight() - actions.position().top + 10,
+  		left:function(){
+  			var pos = (bttnSize-$(this).width())/2;
+  			//if(pos < 1) return 0;
   			return pos;
   		},
   	});
