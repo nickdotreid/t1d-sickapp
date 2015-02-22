02508a8ffdef864ee060b62af9c0d72524d0b058
Nick Reid <nickreid@nickreid.com>
2015-02-22 12:30:49 -0500

    hid icons if screen too small

--- js/main.js
+++ js/main.js
@@ -12,22 +12,35 @@ $( window ).resize(function() {
 
   	var iconsAligned = $('.icon-aligned', actions);
   	if(iconsAligned.length < 0) return 0;
+    var dottedLines = $('.dotted-line',actions);
+
+    var contentHeight = $('.content').outerHeight();
+
+    iconsAligned.show();
+    dottedLines.show();
+    if($(window).height() - contentHeight - actions.height() < iconsAligned.height()){
+      // if there is not enough room for the icons
+      iconsAligned.hide();
+      dottedLines.hide();
+      return;
+    }
+
+    var iconsTop = contentHeight - actions.position().top;
 
   	iconsAligned.css({
   		display:'block',
   		position:'absolute',
-  		top:$('.content').outerHeight() - actions.position().top,
+  		top:iconsTop,
   		left:function(){
   			var pos = (bttnSize-$(this).width())/2;
   			//if(pos < 1) return 0;
   			return pos;
   		},
-  	})
-  	var dottedLines = $('.dotted-line',actions);
+  	});
   	if(dottedLines.length < 1) var dottedLines = $('<div class="dotted-line"></div>').insertAfter(iconsAligned);
   	dottedLines.css({
-  		height:Math.abs($('.content').outerHeight() - actions.position().top) - 10,
-  		top:$('.content').outerHeight() - actions.position().top + 10,
+  		height:Math.abs(iconsTop) - 10,
+  		top:iconsTop + 10, // moving dotted line underneath icon a smidge
   		left:function(){
   			var pos = (bttnSize-$(this).width())/2;
   			//if(pos < 1) return 0;
