f318ceccd3a4b448d7ad9e9a31b68270820d2cf2
Nick Reid <nickreid@nickreid.com>
2015-03-06 15:01:00 -0800

    added backbone hash router and moved layout logic to page.js

---
+++ js/page.js
@@ -0,0 +1,88 @@
+var Pane = Backbone.View.extend({
+	events:{
+		'click a':'hidePane',
+	},
+	initialize: function() {
+		this.render();
+	},
+	render:function(){
+		var pane = this;
+		this.$el.show();
+
+		  this.$('.actions').each(function(){
+		  	var actions = $(this);
+		  	var btns = $('.btn',actions);
+		  	if (btns.length < 1) return 0;
+		  	var btnWidth = 0;
+		  	btns.width('auto').each(function(){
+		  		btnWidth += $(this).width();
+		  	});
+		  	var bttnSize = pane.$('.container', actions).width()/$('.btn',actions).length;
+		  	btns.width(bttnSize-1);
+
+		  	var iconsAligned = pane.$('.icon-aligned', actions);
+		  	if(iconsAligned.length < 0) return 0;
+		    var dottedLines = pane.$('.dotted-line',actions);
+
+		    var contentHeight = pane.$('.content').outerHeight();
+
+		    iconsAligned.show();
+		    dottedLines.show();
+		    if($(window).height() - contentHeight - actions.height() < iconsAligned.height()){
+		      // if there is not enough room for the icons
+		      iconsAligned.hide();
+		      dottedLines.hide();
+		      return;
+		    }
+
+		    var iconsTop = contentHeight - actions.position().top;
+
+		  	iconsAligned.css({
+		  		display:'block',
+		  		position:'absolute',
+		  		top:iconsTop,
+		  		left:function(){
+		  			var pos = (bttnSize-$(this).width())/2;
+		  			//if(pos < 1) return 0;
+		  			return pos;
+		  		},
+		  	});
+		  	if(dottedLines.length < 1) var dottedLines = $('<div class="dotted-line"></div>').insertAfter(iconsAligned);
+		  	dottedLines.css({
+		  		height:Math.abs(iconsTop) - 10,
+		  		top:iconsTop + 10, // moving dotted line underneath icon a smidge
+		  		left:function(){
+		  			var pos = (bttnSize-$(this).width())/2;
+		  			//if(pos < 1) return 0;
+		  			return pos;
+		  		},
+		  	});
+		  });
+	},
+	hidePane: function(event){
+		this.$el.hide();
+	},
+});
+
+var Workspace = Backbone.Router.extend({
+	routes:{
+		"*path":"page", // gotta catch em all
+	},
+	page: function(path){
+		var pane = false;
+		$('#'+path+':first').each(function(){
+			pane = new Pane({
+				el:this,
+			});
+		});
+		if(!pane){
+			pane = new Pane({
+				el:$('.pane:first')[0],
+			});
+		}
+
+
+	}
+});
+var workspace = new Workspace();
+Backbone.history.start();
