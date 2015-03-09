9ee8a799e7513158f7211cc7f538b169166f5877
Nick Reid <nickreid@nickreid.com>
2015-03-08 20:58:53 -0400

    removed dotted line implementation because it was complicating the code more than needed

--- js/page.js
+++ js/page.js
@@ -14,10 +14,7 @@ var Pane = Page.extend({
 	},
 	render:function(){
 		this.$el.show();
-
 		this.center_content();
-//		this.render_action_icons();
-
 		this.$el.hide();
 	},
 	center_content: function(){
@@ -26,13 +23,6 @@ var Pane = Page.extend({
 		available_space -= $('.navbar:first').height();
 		available_space -= this.$('.content').outerHeight();
 		available_space -= this.$('.actions').outerHeight();
-
-		var iconsAligned = this.$('.actions .icon-aligned');
-		if(iconsAligned.length > 0 && available_space > 0){
-			iconsAligned.show();
-			if(iconsAligned.height() >= available_space) available_space -= iconsAligned.height();
-			iconsAligned.hide();
-		}
 
 		if(available_space > 0){
 			this.$('.content').css({
@@ -45,59 +35,6 @@ var Pane = Page.extend({
 				'margin-bottom': '0px',
 			});
 		}
-	},
-	render_action_icons: function(){
-		var pane = this;
-		this.$('.actions').each(function(){
-			var actions = $(this);
-			var btns = $('.btn',actions);
-			if (btns.length < 1) return 0;
-			var btnWidth = 0;
-			btns.width('auto').each(function(){
-				btnWidth += $(this).width();
-			});
-			var bttnSize = pane.$('.container', actions).width()/$('.btn',actions).length;
-			btns.width(bttnSize-1);
-
-			var iconsAligned = pane.$('.icon-aligned', actions);
-			if(iconsAligned.length < 0) return 0;
-			var dottedLines = pane.$('.dotted-line',actions);
-
-			var contentHeight = pane.$('.content').outerHeight();
-
-			iconsAligned.show();
-			dottedLines.show();
-			
-			if($(window).height() - contentHeight - actions.height() < iconsAligned.height()){
-			  // if there is not enough room for the icons
-			  iconsAligned.hide();
-			  dottedLines.hide();
-			  return;
-			}
-
-			var iconsTop = contentHeight - actions.position().top;
-
-			iconsAligned.css({
-				display:'block',
-				position:'absolute',
-				top:iconsTop,
-				left:function(){
-					var pos = (bttnSize-$(this).width())/2;
-					//if(pos < 1) return 0;
-					return pos;
-				},
-			});
-			if(dottedLines.length < 1) var dottedLines = $('<div class="dotted-line"></div>').insertAfter(iconsAligned);
-			dottedLines.css({
-				height:Math.abs(iconsTop) - 10,
-				top:iconsTop + 10, // moving dotted line underneath icon a smidge
-				left:function(){
-					var pos = (bttnSize-$(this).width())/2;
-					//if(pos < 1) return 0;
-					return pos;
-				},
-			});
-		});
 	},
 	enter: function(){
 		this.$el.show();
