9d02ca9aa7e36a0011334668c1c5e0ef0e18c309
Nick Reid <nickreid@nickreid.com>
2015-03-21 10:48:46 -0400

    compiled sprite file from indvidual imagaes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp = require('gulp'),     
+var gulp = require('gulp'), 
+    gulpif = require('gulp-if'),     
     sass = require('gulp-ruby-sass') 
     notify = require("gulp-notify") 
+    sprite = require('css-sprite').stream
     bower = require('gulp-bower');
 
 var config = {
@@ -25,10 +27,21 @@ gulp.task('css', function() { 
              })) 
          .pipe(gulp.dest('./css')); 
 });
+gulp.task('sprites', function () {
+  return gulp.src('./img/sprites/*.png')
+    .pipe(sprite({
+      name: 'sprites',
+      style: '_sprite.scss',
+      cssPath: '../img',
+      processor: 'scss'
+    }))
+    .pipe(gulpif('*.png', gulp.dest('./img/'), gulp.dest('./sass/')))
+});
 
 // Rerun the task when a file changes
  gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+     gulp.watch('./img/sprites/*.png', ['sprites']);
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
 });
 
-  gulp.task('default', ['bower', 'css']);
+  gulp.task('default', ['bower', 'sprites', 'css']);
